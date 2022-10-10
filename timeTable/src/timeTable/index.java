b66f72d3e74a3e69f420bb7ee3da923136538e98
Lenovo <Lenovo@Ankush>
2022-10-10 13:43:13 +0530

    first commit, print 'hello world'

---
+++ timeTable/src/timeTable/index.java
@@ -0,0 +1,16 @@
+ package timeTable;
+
+public class index {
+
+	public int myfun() {
+		
+		
+		return 0;
+	}
+	public static void main(String[] args) {
+	
+		System.out.print("Hello World!!");
+	}
+
+}
+

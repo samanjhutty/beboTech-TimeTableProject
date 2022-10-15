b9afb4a1c5e04090480faf0e74395c7c9529d508
samanjhutty <jhuttysaman@gmail.com>
2022-10-15 22:36:34 +0530

    check commit

--- timeTable/src/timeTable/AddRowColumn.java
+++ timeTable/src/timeTable/AddRowColumn.java
@@ -4,10 +4,11 @@ public class AddRowColumn {
 	int ifColumnAdded=0;
 	
 	private int passingRows() {
+		
 		TableStructure tableStructure=new TableStructure();
 		tableStructure.tableHeading();
-		tableStructure.tableRows(3);//make dynamic
-		tableStructure.tableRows(1);// make dynamic
+		tableStructure.tableRows(i);//make dynamic
+		
 		
 		return 0;	
 	}
